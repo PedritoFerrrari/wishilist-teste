e2137e26408913b82e79b7e71ed4caa0fc72603d
PedritoFerrrari <pedroferrari7309@gmail.com>
2022-06-28 17:12:53 -0300

    agora vai 3

--- server.js
+++ server.js
@@ -2,24 +2,48 @@ const express = require('express');
 const app = express();
 const puppeteer = require('puppeteer');
 
-app.get('/api/formacao-front-end', async (req, res) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://pt.aliexpress.com/item/1005003950006195');
+  const browser =  puppeteer.launch();
+  const page =  browser.newPage();
+  page.goto('https://pt.aliexpress.com/item/1005003950006195');
 
- const pageData = await page.evaluate(() => {
+ const pageData =  page.evaluate(() => {
     return {
       title: document.querySelector('.product-title-text').innerText,
       subtitle: document.querySelector('.product-selling-tags').innerHTML,
     };
   });
 
-  await browser.close();
+   browser.close();
 
-  res.send({
+  console.log({
     "title": pageData.title,
     "subtitle": pageData.subtitle
   })
-});
 
-app.listen(3000); 
+
+
+// const express = require('express');
+// const app = express();
+// const puppeteer = require('puppeteer');
+
+// app.get('/api/formacao-front-end', async (req, res) => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto('https://pt.aliexpress.com/item/1005003950006195');
+
+//  const pageData = await page.evaluate(() => {
+//     return {
+//       title: document.querySelector('.product-title-text').innerText,
+//       subtitle: document.querySelector('.product-selling-tags').innerHTML,
+//     };
+//   });
+
+//   await browser.close();
+
+//   res.send({
+//     "title": pageData.title,
+//     "subtitle": pageData.subtitle
+//   })
+// });
+
+// app.listen(3000); 
